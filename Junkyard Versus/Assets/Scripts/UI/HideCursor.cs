5f9a1c72ff621f1955d0785d3982755fbd91553a
SweetBisc <80738498+SweetBisc@users.noreply.github.com>
2023-02-16 04:16:21 +0800

    Added original art assets
    Capcom lawsuit incomming

---
+++ Junkyard Versus/Assets/Scripts/UI/HideCursor.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideCursor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Set Cursor to not be visible
+        Cursor.visible = false;
+
+        //thats it
+        //pls delete this when its done in other scripts
+    }
+
+}
